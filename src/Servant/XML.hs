f0718ebee3561b92d1b89a44d495613409b6e3f3
Neil Menghani <neil@polimorphic.com>
2023-03-21 15:29:46 -0700

    Use ToXmlDocument

--- src/Servant/XML.hs
+++ src/Servant/XML.hs
@@ -7,43 +7,13 @@ module Servant.XML where
 import Data.Map (empty)
 import Network.HTTP.Media ((//), (/:))
 import Servant.API (Accept, MimeRender, contentType, mimeRender)
-import Text.XML (renderLBS)
-import Text.XML -- REMOVE
-import Text.XML.Marshal (ToXml, toXml)
+import Text.XML (renderLBS, def)
+import Text.XML.Marshal (ToXmlDocument, toXmlDocument)
 
 data XML
 
 instance Accept XML where
     contentType _ = "application" // "xml" /: ("charset", "utf-8")
 
-instance ToXml a => MimeRender XML a where
-    mimeRender _ = renderLBS def . nodesToDocument . toXml
-
-nodesToDocument :: [Node] -> Document
-nodesToDocument ns = case ns of
-    [n] -> case n of
-        NodeElement ne -> Document
-            { documentPrologue = Prologue
-                { prologueBefore = []
-                , prologueDoctype = Nothing
-                , prologueAfter = []
-                }
-            , documentRoot = ne
-            , documentEpilogue = []
-            }
-        _ -> emptyDoc
-    _ -> emptyDoc
-  where
-    emptyDoc = Document
-        { documentPrologue = Prologue
-            { prologueBefore = []
-            , prologueDoctype = Nothing
-            , prologueAfter = []
-            }
-        , documentRoot = Element
-            { elementName = ""
-            , elementAttributes = empty
-            , elementNodes = []
-            }
-        , documentEpilogue = []
-        }
+instance ToXmlDocument a => MimeRender XML a where
+    mimeRender _ = renderLBS def . toXmlDocument
